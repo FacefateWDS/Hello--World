741b8097f86244f790e0d00b5812664c90efe3ad
fate <51080400+FacefateWDS@users.noreply.github.com>
2019-06-12 19:04:05 +0800

    6月12日关于字符串的操作说明

---
+++ 6月12-2.java
@@ -0,0 +1,49 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		String b = null;
+//		// String c=b;
+//		String c = new String(b);// 对于此是空指针，
+//		// 在堆中，而通过null直接初始化的不会，仍然报null
+//		System.out.println(c);
+//		/*
+//		 * Exception in thread "main" java.lang.NullPointerException at
+//		 * java.lang.String.<init>(Unknown Source) at test.Test.main(Test.java:8)
+//		 */
+		
+		
+		
+		
+		
+		
+//		System.out.println(new Test()+22);它尽管有toString()，但并沒有一开始识别为""
+		System.out.println(new Test()+"22");
+		String name=" name";
+		System.out.println(name.equals("NAME"));
+		System.out.println(name.equalsIgnoreCase("Name"));
+		System.out.println(name.endsWith("e"));
+		System.out.println(name.compareTo("a"));//如果大于字符串name,为正数
+		System.out.println(name.startsWith("am",1));//索引是第二个数
+		System.out.println(name.length());//中间空格也算
+		System.out.println(name.toUpperCase());
+		/*
+		 * 字符串的查找字符串看书135页。
+		 * 注意未搜索返回-1
+		 */
+		String n[]=new String[44];
+		System.out.println(name.trim());//去掉首尾空格
+		System.out.println(name.substring(2));//索引取，截取
+		System.out.println(name.replace("n", "m"));//替代
+		System.out.println((n=name.split("a",-1))[1]);//返回发是地址,对于字符串数组
+		//然而数组的里头一部分则是内容
+		
+		
+	
+		
+		
+		
+		
+	}
+}
